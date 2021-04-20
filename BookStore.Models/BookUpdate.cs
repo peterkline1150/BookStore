9c80e08da7c106456be965f2cccc9ac6ad1658e1
peterkline1150 <peterkline1150@gmail.com>
2021-04-20 13:53:53 -0500

    working on models

---
+++ BookStore.Models/BookUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class BookUpdate
+    {
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public int GenreId { get; set; }
+        public int CompanyId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int NumCopies { get; set; }
+
+        public double Price { get; set; }
+    }
+}
