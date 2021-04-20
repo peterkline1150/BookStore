e7544e1a9edcfda01fc4a9d1655cd2c605bd8f79
JustinnZiegler <justinnziegler@gmail.com>
2021-04-20 12:01:21 -0400

    Initial Commit

--- BookStore.Data/Author.cs
+++ BookStore.Data/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace BookStore.Data
 {
     public class Author
     {
+        [Key]
+        public int AuthorId { get; set; }
+
+        [Required]
+        public string AuthorName { get; set; }
+
+        [Required]
+        public DateTime Birthdate { get; set; }
     }
 }
