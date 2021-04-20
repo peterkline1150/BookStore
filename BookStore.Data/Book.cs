d6ccc30e902b955d1a292c36986ef888a8487beb
peterkline1150 <peterkline1150@gmail.com>
2021-04-20 10:59:19 -0500

    setting up peter branch

--- BookStore.Data/Book.cs
+++ BookStore.Data/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,7 @@ namespace BookStore.Data
 {
     public class Book
     {
+        [Key]
+        public int BookId { get; set; }
     }
 }
