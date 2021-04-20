8a8c9629aff33b3ad98983396e6c852b6f7112e4
grimmer.jeremy@gmail.com <grimmer.jeremy@gmail.com>
2021-04-20 13:49:29 -0400

    jeremy models

--- BookStore.Models/CartUpdate.cs
+++ BookStore.Models/CartUpdate.cs
@@ -1,23 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BookStore.Data
+namespace BookStore.Models
 {
-    public class Cart
+    public class CartUpdate
     {
-        [Key]
-        public int CartId { get; set; }
-        [Required]
         public double Tax { get; set; }
-        [Required]
         public double TotalCost { get; set; }
         public string BookList { get; set; }
-        [Required]
-        public Guid BuyerId { get; set; }
-
     }
 }
---
+++ BookStore.Models/GenreList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class GenreList
+    {
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+    }
+}
